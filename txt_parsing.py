d79275fea23167e13714ad43975465e2b884b0a9
Spencer OConnell <oconnesp@tcd.ie>
2025-05-12 17:14:08 +0100

    new build

--- txt_parsing.py
+++ txt_parsing.py
@@ -2,7 +2,7 @@ import re
 import numpy as np
 
 def extract_from_txt (filename, no_trials):
-    responses = []
+    responses = []##response and ISI lists, each contain an array per trial
     ISIs = []
 
     response_pattern = r"Response:\s*(\d(?:,\s*\d)*)"
@@ -10,10 +10,10 @@ def extract_from_txt (filename, no_trials):
 
     with open (filename, "r") as file:
         for line in file:
-            resp_match = re.search (response_pattern, line)
-            ISI_match = re.search (ISI_pattern, line)
+            resp_match = re.search (response_pattern, line)#search for the pattern that is superceded by the responses
+            ISI_match = re.search (ISI_pattern, line)#equivalent for ISIs
             if resp_match:
-                resp_nums = np.array(list(map(int, resp_match.group(1).split(','))))
+                resp_nums = np.array(list(map(int, resp_match.group(1).split(','))))#read the values and add them to the list of responses
                 responses.append(resp_nums)
             if ISI_match:
                 ISI_nums = np.array([float(n) for n in ISI_match.group(1).split(',')]) * 1000
